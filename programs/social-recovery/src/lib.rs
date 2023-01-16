8e6dd9a3dff6595c172beadda9a704e0b0b4a77d
nic <nic.wilson@gmail.com>
2023-01-16 16:52:38 -0600

    feat: ix0 init wallet working

--- programs/social-recovery/src/lib.rs
+++ programs/social-recovery/src/lib.rs
@@ -6,10 +6,32 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod social_recovery {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeRecoveryWallet>, authority: Pubkey) -> Result<()> {
+        ctx.accounts.wallet.authority = authority;
+        ctx.accounts.wallet.bump = *ctx.bumps.get("wallet").unwrap();
         Ok(())
     }
 }
 
+#[account]
+#[derive(Default)]
+pub struct RecoveryWallet {
+    authority: Pubkey,
+    signers: Vec<Pubkey>, // limit of 10
+    bump: u8,
+}
+
+impl RecoveryWallet {
+    pub const MAX_SIZE: usize = 32 + 10 * 32;
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(authority: Pubkey)]
+pub struct InitializeRecoveryWallet<'info> {
+    #[account(init, payer = payer, space = 8 + RecoveryWallet::MAX_SIZE,
+         seeds = [ b"wallet".as_ref(), authority.to_bytes().as_ref()], bump)]
+    pub wallet: Account<'info, RecoveryWallet>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
--- tests/social-recovery.ts
+++ tests/social-recovery.ts
@@ -1,16 +1,43 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { SocialRecovery } from "../target/types/social_recovery";
+import { PublicKey } from "@solana/web3.js";
+import { expect } from "chai";
 
 describe("social-recovery", () => {
+  const provider = anchor.AnchorProvider.env();
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SocialRecovery as Program<SocialRecovery>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const authority = anchor.web3.Keypair.generate();
+    const payer = (program.provider as anchor.AnchorProvider).wallet;
+
+    const [recoveryWalletPDA, _] = PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode("wallet"),
+        authority.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const tx = await program.methods
+      .initialize(authority.publicKey)
+      .accounts({
+        wallet: recoveryWalletPDA,
+        payer: payer.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([])
+      .rpc();
+
+    const walletState = await program.account.recoveryWallet.fetch(
+      recoveryWalletPDA
+    );
+
+    expect(walletState.authority.toString() === authority.publicKey.toString());
   });
 });
